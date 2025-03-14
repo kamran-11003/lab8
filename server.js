951d2aa79bc9d24a6a53d7b503d215f87229ffbb
kamran-11003 <kamranchohan740@gmail.com>
2025-03-14 08:52:43 +0500

    changed port

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
   res.json({ message: 'Welcome to Event Planner API' });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
